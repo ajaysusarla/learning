e5685f85cb292cbff446733515fb289750c7397d
Parthasarathi Susarla <ajaysusarla@gmail.com>
2014-02-25 22:06:14 +1100

    Data structures directory.

---
+++ DS/chapter01/27.c
@@ -0,0 +1,14 @@
+/*
+  27. Write a program for an algorithm that converts a numeric scroe to a letter
+  grade. The grading scale is the rather typical "absolute scale" in which 90%
+  or more is an A, 80% to 90% is a B, 70% to 80% is a C, and 60% to 70% is a D.
+  Anything below 60% is an F.
+ */
+
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+
+	return 0;
+}
