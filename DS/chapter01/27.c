b3b695d6ffd3bb1499097c9ea171eaf673ba4857
Parthasarathi Susarla <ajaysusarla@gmail.com>
2015-01-26 22:29:43 +1100

    Added my implementation of strtol.

--- DS/chapter01/27.c
+++ DS/chapter01/27.c
@@ -7,8 +7,76 @@
 
 #include <stdio.h>
 
+static inline int my_isspace(char c)
+{
+        return (c == ' '  ||
+                c == '\t' ||
+                c == '\n');
+}
+
+static inline int my_isnum(char c)
+{
+        return (c >= '0' && c <= '9');
+}
+
+long my_strtol(const char *str)
+{
+        char *p = (char *) str;
+        long res = 0;
+        int sign = 0, temp = 0;
+
+        while (my_isspace(*p)) {
+                p++;
+        }
+
+        if (*p == '-') {
+                sign = 1;
+                p++;
+        } else if (*p == '+') {
+                sign = 0;
+                p++;
+        }
+
+        while (*p) {
+                if (my_isnum(*p)) {
+                        temp = *p - '0';
+                        res = res*10 + temp;
+                        p++;
+                } else {
+                        res = -999999;
+                        break;
+                }
+        }
+
+        if (sign)
+                res = -res;
+
+        return res;
+}
+
 int main (int argc, char **argv)
 {
+        long score;
 
-	return 0;
+        if (argc != 2) {
+                fprintf(stderr, "Usage: %s <score>\n", argv[0]);
+                return -1;
+        }
+
+        score = my_strtol(argv[1]);
+
+        if (score >= 90)
+                fprintf(stdout, "GRADE: A\n");
+        else if (score >= 80)
+                fprintf(stdout, "GRADE: B\n");
+        else if (score >= 70)
+                fprintf(stdout, "GRADE: C\n");
+        else if (score >= 60)
+                fprintf(stdout, "GRADE: D\n");
+        else if (score < 60 && score > 0)
+                fprintf(stdout, "GRADE: F\n");
+        else
+                fprintf(stdout, "GRADE: FAIL!!\n");
+
+        return 0;
 }
